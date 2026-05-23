b6ed0c11065aa00e7785298f1c41e44acfdb910b
mariamshoukat586-ux <mariamshoukat586@gmail.com>
2026-05-23 14:25:39 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,16 @@
-let display = document.getElementById("display");
-let buttons = document.querySelectorAll("button");
+function appendValue(value) {
+  document.getElementById("display").value += value;
+}
 
-let currentInput = "";
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
 
-buttons.forEach(button => {
-  button.addEventListener("click", () => {
-
-    let value = button.innerText;
-
-    if(value === "AC"){
-      currentInput = "";
-      display.value = "";
-    }
-
-    else if(value === "DEL"){
-      currentInput = currentInput.slice(0, -1);
-      display.value = currentInput;
-    }
-
-    else if(value === "="){
-      try{
-        currentInput = eval(
-          currentInput
-          .replace("×", "*")
-          .replace("÷", "/")
-        ).toString();
-
-        display.value = currentInput;
-      }
-      catch{
-        display.value = "Error";
-      }
-    }
-
-    else{
-      currentInput += value;
-      display.value = currentInput;
-    }
-
-  });
-});
+function calculate() {
+  try {
+    document.getElementById("display").value =
+      eval(document.getElementById("display").value);
+  } catch {
+    document.getElementById("display").value = "Error";
+  }
+}
